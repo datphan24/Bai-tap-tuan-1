ea6c337f12ff396ca919723fa09957b173bd4e98
DatPhan <tt.dat.phan@tomosia.com>
2022-04-05 17:20:22 +0700

    delete a todo, update a todo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,12 @@
-let inputContent = document.querySelector('.add-input');
-let form = document.querySelector('form');
-let todos = document.querySelector('.list-todo');
+const inputContent = document.querySelector('.add-content .add-input');
+const form = document.querySelector('form');
+const todos = document.querySelector('.list-todo');
+const all = 'all';
+const active = 'active';
+const completed = 'completed';
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
     let contentValue = inputContent.value.trim();
     if(contentValue) {
         addTodoElement({
@@ -13,23 +16,78 @@ form.addEventListener('submit', function(event) {
     inputContent.value = '';
 })
 function addTodoElement(todo) {
-    var li = document.createElement('li');
+    let liTodo = document.createElement('li');
     
-    li.innerHTML = `
-        <span>${todo.text}</span>
-        <span><i class="fa fa-times-circle" aria-hidden="true"></i></span>
+    liTodo.innerHTML = `
+        <span class="">${todo.text}</span>
+        <input
+            type="text"
+            value="${todo.text}"
+            class="add-input hidden"
+        />
+        <span>
+              <i class="fa fa-times-circle" aria-hidden="true"></i>
+        </span>
     `
-    li.setAttribute('class', 'item-todo general-size')
-    li.querySelector('span:first-child').addEventListener('click', function(e) {
+    
+    liTodo.setAttribute('class', 'item-todo general-size')
+    //delete a todo
+    liTodo.querySelector('span:last-child')
+        .addEventListener('click', function (e) {
+            this.parentElement.remove()
+        })
+    
+    var spanTodo = liTodo.querySelector('span:first-child')
+    // tick todo completed
+    spanTodo.addEventListener('click', function(e) {
         this.classList.toggle('completed')
     })
-    li.querySelector('span:last-child').addEventListener('click', function(e) {
-        this.parentElement.remove()
+    // edit todo
+    spanTodo.addEventListener('dblclick', function(e) {
+        this.classList.add('hidden')
+        if (spanTodo.classList.contains('hidden')) {
+            let editTodo = liTodo.querySelector('.add-input')
+            editTodo.classList.remove('hidden')
+            editTodo.addEventListener('keyup', function (e) {
+                if (e.keyCode === 13) {
+                    e.preventDefault();
+                    spanTodo.innerText = editTodo.value.trim()
+                    spanTodo.classList.remove('hidden')
+                    editTodo.classList.add('hidden')
+                }
+            })
+        }  
     })
-    todos.appendChild(li)
+    
+    todos.appendChild(liTodo)
 }
+
 function getActive() {
-  let buttonAll = document.getElementById('all')
-  let buttonActive = document.getElementById('active')
-  let buttonCompleted = document.getElementById('completed')
+    let buttonAll = document.getElementById(all);
+    let buttonActive = document.getElementById(active);
+    let buttonCompleted = document.getElementById(completed);
+    let listTodo = document.querySelectorAll('.item-todo')
+
+    buttonAll.addEventListener('click', function() {
+        this.classList.add('on')
+        buttonActive.classList.remove('on')
+        buttonCompleted.classList.remove('on')
+    })
+    buttonActive.addEventListener('click', function() {
+        this.classList.add('on')
+        buttonAll.classList.remove('on')
+        buttonCompleted.classList.remove('on')
+        listTodo.forEach(item => {
+            // if (item.querySelector('span:first-child').classList.contains('hidden')) {
+            //     item.classList.add('hidden')
+            // }
+            console.log(item)
+        })
+    })
+    buttonCompleted.addEventListener('click', function() {
+        this.classList.add('on')
+        buttonAll.classList.remove('on')
+        buttonActive.classList.remove('on')
+    })
 }
+getActive()
